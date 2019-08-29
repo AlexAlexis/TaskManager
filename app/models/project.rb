ff0a26ba255480110388707138c46c0f5a0444b8
Alex <np.zaporoje.sb2.com>
2019-08-29 21:57:34 +0300

    relations between user -> project -> task have been installed

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
+  belongs_to :user
+  has_many :tasks
   scope :newest_first, -> {order("created_at DESC")}
   scope :newest_two, -> {order("created_at  DESC").limit(2)}
 end
